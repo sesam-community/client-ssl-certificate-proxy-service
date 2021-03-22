314c75ed495e3ab4006a16a587707f9c85e2067f
Tarjei Skrede <tarjeiskrede@M-C02Y7599JGH6.local>
2021-03-22 11:11:08 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,60 @@
+## Client side SSL certificate proxy service
+
+A simple service that serves as a proxy for signing request calls to an API with a client side SSL certificate (PFX).
+
+[![SesamCommunity CI&CD](https://github.com/sesam-community/client-ssl-certificate-proxy-service/actions/workflows/sesam-community-ci-cd.yml/badge.svg)](https://github.com/sesam-community/client-ssl-certificate-proxy-service/actions/workflows/sesam-community-ci-cd.yml)
+
+### Environment variables:
+
+`base_url` - the base url of the API service.
+
+`certificate` - the certificate content of the PFX file.
+
+`private_key` - the private key content of the PFX file.
+
+`username` - username to the API service.
+
+`password` - password to the API service.
+
+
+### Example system config:
+
+```json
+{
+  "_id": "client-ssl-certificate-proxy-system",
+  "type": "system:microservice",
+  "docker": {
+    "environment": {
+      "base_url": "https://base.url.to.api/",
+      "certificate": "$SECRET(pfx_certificate)",
+      "private_key": "$SECRET(pfx_privatekey)",
+      "username": "username-to-api",
+      "password": "$SECRET(secret-password-to-api)",
+      "page_number": "1",
+      "page_size": "1000",
+      "protocol": "json"
+    },
+    "image": "sesamcommunity/client-ssl-certificate-proxy-service:latest",
+    "port": 5001
+  }
+}
+
+```
+
+### Example pipe using the microservice above
+
+```json
+{
+  "_id": "client-ssl-certificate-proxy-pipe",
+  "type": "pipe",
+  "source": {
+    "is_chronological": false,
+    "is_since_comparable": true,
+    "supports_since": false,
+    "system": "client-ssl-certificate-proxy-system",
+    "type": "json",
+    "url": "api-path"
+  }
+}
+
+```
---
+++ service/client-ssl-certificate-proxy-service.py
@@ -0,0 +1,76 @@
+import json
+import requests
+from flask import Flask, Response
+import os
+import logger
+import cherrypy
+
+app = Flask(__name__)
+logger = logger.Logger('client-ssl-certificate-proxy-service')
+pkey_file = 'pkey.pem'
+cert_file = 'cert.pem'
+ca_file = 'ca.pem'
+
+url = os.environ.get("base_url")
+username = os.environ.get("username")
+pw = os.environ.get("password")
+cert = os.environ.get("certificate").replace(r'\n', '\n')
+pkey = os.environ.get("private_key").replace(r'\n', '\n')
+
+
+def write_certificate():
+    open(pkey_file, 'wb').write(bytes(pkey, 'ascii'))
+    open(cert_file, 'wb').write(bytes(cert, 'ascii'))
+
+
+def stream_json(clean):
+    first = True
+    yield '['
+    for i, row in enumerate(clean):
+        if not first:
+            yield ','
+        else:
+            first = False
+        yield json.dumps(row)
+    yield ']'
+
+
+@app.route("/<path:path>", methods=["GET"])
+def get(path):
+    request_url = "{0}{1}".format(url, path)
+    request_cert = (cert_file, pkey_file)
+
+    logger.info("Request url: %s", request_url)
+
+    try:
+        request_data = requests.get(request_url, auth=(username, pw), cert=request_cert).text
+        logger.info("Data received: %s:", request_data)
+        entities = json.loads(request_data)
+    except Exception as e:
+        logger.warning("Exception occurred when download data from '%s': '%s'", request_url, e)
+        logger.warning("Data retrieved from service: %s", request_data)
+        raise
+
+    return Response(
+            stream_json(entities),
+            mimetype='application/json'
+        )
+
+
+if __name__ == '__main__':
+    write_certificate()
+
+    cherrypy.tree.graft(app, '/')
+
+    # Set the configuration of the web server to production mode
+    cherrypy.config.update({
+        'environment': 'production',
+        'engine.autoreload_on': False,
+        'log.screen': True,
+        'server.socket_port': 5002,
+        'server.socket_host': '0.0.0.0'
+    })
+
+    # Start the CherryPy WSGI web server
+    cherrypy.engine.start()
+    cherrypy.engine.block()
